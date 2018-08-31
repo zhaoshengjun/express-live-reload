55f2a6c4c6672b7f3e4f4e378282d6fd3b5bcbf4
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-31 11:59:22 +1000

    dynamic inject

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
+const {
+    readFileSync
+} = require("fs");
+const livereload = require("livereload");
+const cheerio = require("cheerio");
+
+const server = livereload.createServer();
+server.watch(__dirname);
 
 const app = express();
 const PORT = 8800;
 
-app.use(express.static(__dirname))
+const SCRIPT = `<script>
+    document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] +
+    ':35729/livereload.js?snipver=1"></' + 'script>')
+  </script>`;
+app.use(/\/index|\/|/, (req, res) => {
+    let file = readFileSync("./index.html");
+    let doc = cheerio.load(file);
+    console.log("before:", doc.html());
+    doc("body").append(SCRIPT);
+    console.log("after:", doc.html());
+    res.send(doc.html());
+});
+app.use(express.static(__dirname));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+    console.log(`Server is running on port ${PORT}`);
+});
